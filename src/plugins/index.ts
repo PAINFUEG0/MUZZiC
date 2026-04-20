8247fd2e5b867b916d1b94b5f77ec622d27a4f98
painfuego <kystax69@gmail.com>
2026-04-20 19:47:09 +0530

    follow up

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -1,3 +1,4 @@
-export * from "./lyrics/index.js";
 export * from "./source/saavn/index.js";
 export * from "./source/tidal/index.js";
+export * from "./lyrics/source/apple.js";
+export * from "./lyrics/source/lrclib.js";
--- src/plugins/lyrics/index.ts
+++
@@ -1,2 +0,0 @@
-export * from "./source/apple.js";
-export * from "./source/lrclib.js";
