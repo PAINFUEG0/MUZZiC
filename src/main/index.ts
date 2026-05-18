b612f1209a6dc8cf7e529a1316f49a61419263a9
painfuego <kystax69@gmail.com>
2026-05-18 20:07:49 +0530

    upd

--- src/main/index.ts
+++ src/main/index.ts
@@ -4,25 +4,38 @@ import ffbinaries from "ffbinaries";
 import { promisify } from "node:util";
 import { exec } from "node:child_process";
 
-process.env.DLP = "./bin/yt-dlp";
-process.env.FFMPEG = "./bin/ffmpeg";
-process.env.FFPROBE = "./bin/ffprobe";
-
-export const WIN32 = process.platform === "win32";
-export const YT_DLP_BIN_URL = `https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp${WIN32 ? ".exe" : ""}`;
-
-export const execute = promisify(exec);
+const execute = promisify(exec);
+const WIN32 = process.platform === "win32";
+process.env.DLP = `./bin/yt-dlp${WIN32 ? ".exe" : ""}`;
+process.env.FFMPEG = `./bin/ffmpeg${WIN32 ? ".exe" : ""}`;
+process.env.FFPROBE = `./bin/ffprobe${WIN32 ? ".exe" : ""}`;
+const YT_DLP_BIN_URL = `https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp${WIN32 ? ".exe" : ""}`;
 
 fs.existsSync("./bin") || fs.mkdirSync("./bin");
 
-if (!fs.existsSync(process.env.DLP))
+console.log("Checking for yt-dlp binary");
+if (!fs.existsSync(process.env.DLP)) {
+  console.log("Missing yt-dlp binary, downloading . . .");
   await fs.promises
     .writeFile(`${process.env.DLP}${!WIN32 || ".exe"}`, Buffer.from((await axios(YT_DLP_BIN_URL, { responseType: "arraybuffer" })).data))
-    .then(async () => void (!WIN32 && (await execute(`chmod +x ${process.env.DLP}`))));
+    .then(async () => (console.log("yt-dlp binary downloaded"), !WIN32 && (await execute(`chmod +x ${process.env.DLP}`))))
+    .catch(() => console.log("Failed to download yt-dlp binary"));
+} else console.log("PASS : Found yt-dlp binary");
 
-if (!fs.existsSync(process.env.FFMPEG) || !fs.existsSync(process.env.FFPROBE))
-  await new Promise((resolve, reject) =>
-    ffbinaries.downloadBinaries(["ffmpeg", "ffprobe"], { destination: "./bin" }, (err, data) => (err ? reject(err) : resolve(data))),
-  );
+console.log("Checking for ffmpeg binary");
+if (!fs.existsSync(process.env.FFMPEG)) {
+  console.log("Missing ffmpeg binary, downloading . . .");
+  await promisify(ffbinaries.downloadBinaries.bind(ffbinaries, ["ffmpeg"], { destination: "./bin" }))()
+    .then(() => console.log("ffmpeg binary downloaded"))
+    .catch(() => console.log("Failed to download ffmpeg binary"));
+} else console.log("PASS : Found ffmpeg binary");
+
+console.log("Checking for ffprobe binary");
+if (!fs.existsSync(process.env.FFPROBE)) {
+  console.log("Missing ffmpeg binary, downloading . . .");
+  await promisify(ffbinaries.downloadBinaries.bind(ffbinaries, ["ffprobe"], { destination: "./bin" }))()
+    .then(() => console.log("ffprobe binary downloaded"))
+    .catch(() => console.log("Failed to download ffprobe binary"));
+} else console.log("PASS : Found ffprobe binary");
 
 await import("./local.test.js");
--- src/main/local.test.ts
+++ src/main/local.test.ts
@@ -1,87 +1,26 @@
-import * as fs from "node:fs";
-import * as path from "node:path";
-import { chunk } from "../shared/helpers.js";
-import { CoreDatabase } from "@xenodb/server";
+import path from "path";
 import { DirNode } from "../shared/types/utils.js";
-import { Track } from "../shared/types/sourcePlugin.js";
-import { walk, finger, probe, thumb } from "../plugins/local/index.js";
+import { LocalResourceProvider } from "../plugins/local/index.js";
 
-const perChunk = 10;
-const dir = "../Projects/AMDL/random";
-// const dir = "../Projects/AMDL/downloads";
+console.log("Initializing Local Resource Provider");
 
-const tracks = new CoreDatabase<any>("./database/tracks/tracks");
-const defaultThumbnail = "https://i.ytimg.com/vi/dQw4w9WgXcQ/hqdefault.jpg";
+const dir = "../Music/old";
+const provider = new LocalResourceProvider();
 
-const index = new CoreDatabase<DirNode<boolean>>("./database/tracks/index");
+await provider.init();
 
-console.log("Database initialized");
+console.log("Scanning directory (%s)", dir);
+const unIndexedTree = (await provider.scan(dir)) || ({ name: path.basename(dir), path: dir, files: [], dirs: [] } as DirNode<false>);
+provider.tree.set("local", unIndexedTree);
 
-console.log("Walking directory (%s)", dir);
-const walkStart = performance.now();
-const tree = await walk(dir);
-if (!tree) throw new Error("Tree is empty");
-index.set("tree", tree);
-console.log("Tree generated in %d ms", performance.now() - walkStart);
+console.log("Fingering directory (%s)", dir);
+const indexedTree = await provider.finger(unIndexedTree);
+provider.tree.set("local", indexedTree);
 
-console.log("Fingering tree");
-const fingerStart = performance.now();
-const indexedTree = await finger(tree);
-index.set("tree", indexedTree);
-const flat = flatten(indexedTree);
-console.log("Fingerprinting completed in %d ms (%d files)", performance.now() - fingerStart, flat.length);
+console.log("Probing directory (%s)", dir);
+await provider.probe(indexedTree);
 
-console.log("Probing files");
-const fileChunks = chunk(flat, perChunk);
-const probeStart = performance.now();
-
-const data: Parameters<typeof tracks.setMany>[0] = [];
-for (let i = 0; i < fileChunks.length; i++) {
-  console.log("Probing chunk %d/%d", i + 1, fileChunks.length);
-  const start = performance.now();
-  await Promise.all(
-    fileChunks[i]!.map((file) =>
-      probe(file.path).then((meta) =>
-        data.push({
-          key: file.id,
-          value: {
-            id: file.id,
-            streamURI: file.path,
-            thumb: `${file.id}.jpg`,
-            explicit: meta.explicit,
-            duration: meta.duration,
-            lyrics: "No lyrics found",
-            resolution: meta.resolution,
-            title: path.basename(file.path, path.extname(file.path)),
-            album: { name: meta.album, id: "0", thumb: `${file.id}.jpg` },
-            artists: meta.artists.map((name) => ({ name, thumb: "", id: "" })),
-          } satisfies Track<true>,
-        }),
-      ),
-    ),
-  );
-  console.log("Probed (%d files) in %d ms", fileChunks[i]?.length, performance.now() - start);
-}
-tracks.setMany(data);
-console.log("Probing completed in %d ms", performance.now() - probeStart);
-
-const failed: string[] = [];
-if (!fs.existsSync("./.thumbnails")) fs.mkdirSync("./.thumbnails");
-
-console.log("Extracting thumbnails");
-const thumbStart = performance.now();
-for (let i = 0; i < fileChunks.length; i++) {
-  const start = performance.now();
-  await Promise.all(fileChunks[i]!.map((file) => thumb(file.path, `./.thumbnails/${file.id}.jpg`).catch(() => failed.push(file.id))));
-  console.log("Extracted thumbnails (%d files) in %d ms", fileChunks[i]?.length, performance.now() - start);
-}
-tracks.setMany(tracks.getMany(failed).map((track) => ({ key: track.id, value: { ...track, thumb: defaultThumbnail } })));
-console.log("Thumbnail extraction completed in %d ms", performance.now() - thumbStart);
-console.log("Failed to extract thumbnails for %d files", failed.length);
-
-function flatten<T extends boolean>(node: DirNode<T>): DirNode<T>["files"] {
-  if (!node) return [];
-  return [...node.files, ...node.dirs.flatMap(flatten)];
-}
+console.log("Building thumbnails (%s)", dir);
+await provider.buildThumbnails(indexedTree);
 
 process.on("SIGINT", () => setTimeout(() => process.exit(), 500));
