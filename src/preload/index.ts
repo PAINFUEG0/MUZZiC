59e37a853b83584daf5d34b88538f558cf8f3c15
painfuego <kystax69@gmail.com>
2026-05-22 16:33:24 +0530

    nothing notable

--- src/preload/index.ts
+++ src/preload/index.ts
@@ -1,7 +1,5 @@
-import { readdirSync } from "node:fs";
 import { contextBridge } from "electron";
 
 contextBridge.exposeInMainWorld("api", {
   ping: () => "pong",
-  list: () => readdirSync("."),
 });
--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -2,16 +2,7 @@ import "./index.css";
 import { createRoot } from "react-dom/client";
 
 function Hello() {
-  return (
-    <div className="flex flex-col items-center justify-center h-screen text-red-500">
-      {
-        //@ts-ignore
-        window.api.list().map((e) => (
-          <div>{e}</div>
-        ))
-      }
-    </div>
-  );
+  return <div>Hello </div>;
 }
 
 createRoot(document.getElementById("root")!).render(<Hello />);
