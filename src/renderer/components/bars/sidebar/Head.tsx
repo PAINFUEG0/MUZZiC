c4d584f38d14a8a5439f728217e401ceda8d93df
painfuego <kystax69@gmail.com>
2026-06-18 09:55:59 +0530

    upd head

--- src/renderer/components/bars/sidebar/Head.tsx
+++ src/renderer/components/bars/sidebar/Head.tsx
@@ -4,7 +4,9 @@ import { RiMusic2Fill } from "react-icons/ri";
 
 export function Head() {
   return (
-    <div className="relative flex h-20 w-65 shrink-0 flex-row items-center justify-center gap-1 bg-(--theme)">
+    <div className="relative flex h-20 w-65 shrink-0 flex-row items-center justify-center gap-1">
+      <div className="absolute -z-1 h-full w-full bg-(--theme)/10" />
+
       <RiMusic2Fill className="text-lg" />
       <span className="font-medium">MUZZiC</span>
     </div>
