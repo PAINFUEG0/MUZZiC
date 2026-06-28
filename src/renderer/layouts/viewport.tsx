8ca65dd10f7e2e3b6b698da4de0cec13ebc0dde8
painfuego <kystax69@gmail.com>
2026-06-28 15:29:34 +0530

    album

--- src/renderer/layouts/viewport.tsx
+++ src/renderer/layouts/viewport.tsx
@@ -2,10 +2,12 @@
 
 import { List } from "../pages/list";
 import { Tracks } from "../pages/Tracks";
+import { Albums } from "../pages/Albums";
 import { view } from "../utils/globalStores";
 
 export function Viewport() {
   const [_] = view.use();
   if (_.scene === "tracks") return <Tracks />;
   if (_.scene === "explorer") return <List />;
+  if (_.scene === "albums") return <Albums />;
 }
