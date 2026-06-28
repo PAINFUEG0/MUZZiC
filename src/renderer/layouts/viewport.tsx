feee62676234c0529a8562041a188dd09d5f0537
painfuego <kystax69@gmail.com>
2026-06-28 15:56:59 +0530

    animate page switches

--- src/renderer/layouts/viewport.tsx
+++ src/renderer/layouts/viewport.tsx
@@ -5,11 +5,40 @@ import { Tracks } from "../pages/Tracks";
 import { Albums } from "../pages/Albums";
 import { Artists } from "../pages/Artists";
 import { view } from "../utils/globalStores";
+import { AnimatePresence, motion } from "framer-motion";
 
 export function Viewport() {
-  const [_] = view.use();
-  if (_.scene === "tracks") return <Tracks />;
-  if (_.scene === "explorer") return <List />;
-  if (_.scene === "albums") return <Albums />;
-  if (_.scene === "artists") return <Artists />;
+  let page;
+  const [state] = view.use();
+
+  switch (state.scene) {
+    case "tracks":
+      page = <Tracks />;
+      break;
+    case "explorer":
+      page = <List />;
+      break;
+    case "albums":
+      page = <Albums />;
+      break;
+    case "artists":
+      page = <Artists />;
+      break;
+    default:
+      page = <List />;
+  }
+
+  return (
+    <AnimatePresence mode="wait">
+      <motion.div
+        children={page}
+        key={state.scene}
+        exit={{ opacity: 0, x: -10 }}
+        animate={{ opacity: 1, x: 0 }}
+        transition={{ duration: 0.2 }}
+        initial={{ opacity: 0, x: 10 }}
+        className="flex h-full w-full overflow-hidden"
+      />
+    </AnimatePresence>
+  );
 }
