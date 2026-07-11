a45ce5fe423ef29536317a9ce9a550b2ccadac30
painfuego <kystax69@gmail.com>
2026-07-11 17:20:43 +0530

    follow up

--- src/renderer/pages/Settings.tsx
+++ src/renderer/pages/Settings.tsx
@@ -45,7 +45,7 @@ export function Settings() {
                 const switchTheme = () => {
                   setTheme(theme);
                   localStorage.setItem("theme", i.toString());
-                  document.documentElement.style.setProperty("--accent-color", theme.color);
+                  document.documentElement.style.setProperty("--accent-color", theme.accent);
                   document.documentElement.style.setProperty("--text-color", theme.type === "dark" ? "#ffffff" : "#000000");
                   document.documentElement.style.setProperty("--hover-color", theme.type === "dark" ? "#000000" : "#ffffff");
                   document.documentElement.style.setProperty("--border-color", theme.type === "dark" ? "#ffffff" : "#000000");
