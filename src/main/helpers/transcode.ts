2e8c80c14e1f5ad26ed8313235bd33bf8b851e20
execaman <151807496+execaman@users.noreply.github.com>
2026-07-25 00:59:29 +0530

    feat: single op for transcode()

--- src/main/helpers/transcode.ts
+++ src/main/helpers/transcode.ts
@@ -6,27 +6,31 @@ import { spawn } from "child_process";
 import { randomUUID } from "node:crypto";
 import { getPcmFormat } from "./settings";
 import { directories, bin } from "../constants";
+import type { ChildProcessByStdio } from "node:child_process";
 
 let last = "";
+let child: ChildProcessByStdio<null, null, null> | undefined;
 
 export async function transcode(input: string) {
+  child?.kill("SIGTERM");
+
   const format = await getPcmFormat();
   const out = path.resolve(directories.temp, `${randomUUID()}.wav`);
 
   const res = new Promise<string>((resolve, reject) => {
     const args = ["-i", input, "-map", "0:a:0", "-map_metadata", "-1", "-map_chapters", "-1", "-c:a", format, out];
-    const child = spawn(bin.ffmpeg, args, { stdio: ["ignore", "ignore", "pipe"] });
-    let stderr = "";
+    child = spawn(bin.ffmpeg, args, { stdio: ["ignore", "ignore", "ignore"] });
 
-    child.on("error", reject);
+    child.on("error", () => {
+      child?.kill("SIGTERM");
+    });
 
-    child.stderr.on("data", (chunk) => (stderr += chunk.toString()));
-
-    child.on("close", (code) =>
+    child.on("close", (code) => {
+      child?.kill("SIGTERM");
       code !== 0
-        ? reject(new Error(`ffmpeg exited with code ${code}\n${stderr}`))
-        : resolve("file:///" + encodeURIComponent(out.replace(/\\/g, "/")).replace(/%2F/g, "/")),
-    );
+        ? reject(new Error(`ffmpeg exited with code ${code}`))
+        : resolve("file:///" + encodeURIComponent(out.replace(/\\/g, "/")).replace(/%2F/g, "/"));
+    });
   });
 
   last && (await rm(last, { force: true }).catch(() => null));
