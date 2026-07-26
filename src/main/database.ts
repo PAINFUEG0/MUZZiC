24ed1e7b3c4fbdc9ae1e58ab826e33ea8ba76e1d
painfuego <kystax69@gmail.com>
2026-07-26 12:14:51 +0530

    rem cached and re-push

--- src/main/database.ts
+++
@@ -1,9 +0,0 @@
-/** @format */
-
-import { directories } from "./constants";
-import { CoreDatabase } from "@xenodb/server";
-import { DirNode, BaseTrack } from "../shared/types";
-
-export const tree = new CoreDatabase<DirNode>(`${directories.database}/tree`);
-export const settings = new CoreDatabase(`${directories.database}/settings`);
-export const meta = new CoreDatabase<BaseTrack>(`${directories.database}/meta`);
--- src/preload/index.ts
+++
@@ -1,5 +0,0 @@
-/** @format */
-
-import eˉ from "electron";
-
-eˉ.contextBridge.exposeInMainWorld("invoke", eˉ.ipcRenderer.invoke.bind(eˉ.ipcRenderer));
