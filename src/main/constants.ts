adc3ef96323ef283932787ebe3a9fb84f3b4d07d
painfuego <kystax69@gmail.com>
2026-07-27 19:24:31 +0530

    more codecs and containers

--- src/main/constants.ts
+++ src/main/constants.ts
@@ -14,6 +14,19 @@ export const directories = {
   thumbnails: path.resolve(wd, "thumbnails"),
 };
 
+export const SURROUND_HIRES = [".dts", ".thd", ".mlp", ".dsf", ".dff"];
+export const VIDEO = [".mp4", ".m4v", ".mov", ".mkv", ".webm", ".avi", ".flv", ".wmv"];
+export const LOSSLESS_AUDIO = [".flac", ".alac", ".wav", ".wv", ".ape", ".tak", ".tta"];
+export const AUDIO_ONLY = [".m4b", ".m4p", ".mka", ".oga", ".aiff", ".aif", ".au", ".caf", ".amr"];
+export const LOSSY_AUDIO = [".mp3", ".aac", ".m4a", ".ogg", ".opus", ".wma", ".mp2", ".ac3", ".ac4"];
+
+export const SURROUND = new Set(["ac3", "eac3", "dts", "dtshd", "truehd", "mlp"]);
+export const DSD = new Set(["dsd_lsbf", "dsd_msbf", "dsd_lsbf_planar", "dsd_msbf_planar"]);
+export const LOSSY = new Set(["mp3", "aac", "opus", "vorbis", "wmav1", "wmav2", "mp2", "ac4"]);
+export const LOSSLESS = new Set(["flac", "alac", "pcm_s16le", "pcm_s24le", "pcm_s32le", "wavpack", "ape", "tak"]);
+
+export const AUDIO_EXTENSIONS = new Set([...VIDEO, ...AUDIO_ONLY, ...LOSSY_AUDIO, ...SURROUND_HIRES, ...LOSSLESS_AUDIO]);
+
 fs.existsSync(directories.temp) &&
   fs.readdirSync(directories.temp).forEach((f) => fs.rmSync(path.resolve(directories.temp, f), { force: true }));
 for (const directory of Object.values(directories)) if (!fs.existsSync(directory)) fs.mkdirSync(directory, { recursive: true });
@@ -32,5 +45,3 @@ export const bin = {
     remoteResourceURI: `https://github.com/eugeneware/ffmpeg-static/releases/download/b6.1.1/ffprobe-${process.platform}-${process.arch}`,
   },
 };
-
-export const AUDIO_EXTENSIONS = new Set([".mp3", ".wav", ".ogg", ".flac", ".m4a", ".aac", ".opus", ".webm"]);
