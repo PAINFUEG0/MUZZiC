bc4a8b92c40f14c721862e74f3e5d41b1f12dc68
painfuego <kystax69@gmail.com>
2026-07-28 21:19:31 +0530

    KUP KDN (VOL - 1-100 - PRE/POST)

--- src/renderer/components/playbar/Volume.tsx
+++ src/renderer/components/playbar/Volume.tsx
@@ -1,16 +1,31 @@
 /** @format */
 
-import { memo, useState } from "react";
 import Slider from "../utils/Slider";
 import { playerEffects } from "../../player";
+import { memo, useEffect, useState } from "react";
 import { LuVolumeOff, LuVolume, LuVolume1, LuVolume2 } from "react-icons/lu";
-import { logToPercent, percentToLog } from "../../../shared/helpers";
 
-type T = ReturnType<(typeof playerEffects)["use"]>;
+type _ = ReturnType<(typeof playerEffects)["use"]>;
 
-export const Volume = memo(({ FX, setFX }: { FX: T[0]; setFX: T[1] }) => {
+export const Volume = memo(({ FX, setFX }: { FX: _[0]; setFX: _[1] }) => {
   const [T, setT] = useState<"IG" | "PG">("PG");
-  const U = logToPercent(FX[T]);
+
+  useEffect(() => {
+    const fn = (e: KeyboardEvent) => {
+      const el = e.target as HTMLElement;
+      if (el.isContentEditable || el.tagName === "TEXTAREA" || el.tagName === "INPUT") return;
+
+      const keybinds: Record<string, () => void> = {
+        ArrowUp: () => (e.preventDefault(), setFX((_) => ({ ..._, [T]: Math.min(100, _[T] + 5) }))),
+        ArrowDown: () => (e.preventDefault(), setFX((_) => ({ ..._, [T]: Math.max(0, _[T] - 5) }))),
+      };
+
+      e.code in keybinds && keybinds[e.code]!();
+    };
+
+    window.addEventListener("keydown", fn);
+    return () => window.removeEventListener("keydown", fn);
+  }, [T]);
 
   return (
     <div className="flex h-fit flex-col">
@@ -21,11 +36,11 @@ export const Volume = memo(({ FX, setFX }: { FX: T[0]; setFX: T[1] }) => {
       </div>
 
       <div className="flex h-fit w-fit flex-row items-center gap-1.5">
-        {FX.mute || U === 0 ? (
+        {FX.mute || FX[T] === 0 ? (
           <button children={<LuVolumeOff className="w-5 cursor-pointer" onClick={() => setFX((s: any) => ({ ...s, mute: false }))} />} />
-        ) : U > 0 && U <= 30 ? (
+        ) : FX[T] > 0 && FX[T] <= 30 ? (
           <button children={<LuVolume className="w-5 cursor-pointer" onClick={() => setFX((s: any) => ({ ...s, mute: true }))} />} />
-        ) : U > 30 && U <= 60 ? (
+        ) : FX[T] > 30 && FX[T] <= 60 ? (
           <button children={<LuVolume1 className="w-5 cursor-pointer" onClick={() => setFX((s: any) => ({ ...s, mute: true }))} />} />
         ) : (
           <button children={<LuVolume2 className="w-5 cursor-pointer" onClick={() => setFX((s: any) => ({ ...s, mute: true }))} />} />
@@ -34,14 +49,14 @@ export const Volume = memo(({ FX, setFX }: { FX: T[0]; setFX: T[1] }) => {
         <Slider
           min={0}
           step={1}
-          max={120}
-          value={U}
+          max={100}
+          value={FX[T]}
           className="h-1 w-25"
           thumb={{ height: "0.70rem", width: "0.70rem" }}
-          onChange={(e) => setFX((s: any) => ({ ...s, [T]: percentToLog(Number(e.target.value)) }))}
+          onChange={(e) => setFX((s: any) => ({ ...s, [T]: Number(e.target.value) }))}
         />
 
-        <div className="flex w-7 flex-row items-center justify-start text-xs">{U}%</div>
+        <div className="flex w-7 flex-row items-center justify-start text-xs">{FX[T]}%</div>
       </div>
     </div>
   );
