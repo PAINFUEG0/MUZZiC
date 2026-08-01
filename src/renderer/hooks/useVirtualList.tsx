f56489e9d54eaf65b5c6f23a9278a406f3d37cd9
painfuego <kystax69@gmail.com>
2026-08-01 11:08:21 +0530

    rem item-removal animation in vList to reduce memory spike when scrolling

--- src/renderer/hooks/useVirtualList.tsx
+++ src/renderer/hooks/useVirtualList.tsx
@@ -2,7 +2,6 @@
 
 import { useBlurMask } from "./useBlurMask";
 import { RiErrorWarningLine } from "react-icons/ri";
-import { AnimatePresence, motion } from "framer-motion";
 import { useVirtualizer } from "@tanstack/react-virtual";
 import { ComponentType, ReactNode, RefObject, useEffect } from "react";
 
@@ -30,29 +29,24 @@ export function useVirtualList({ list, scrollRef, getItemKey, Component, oversca
 
   const component = (
     <div style={{ height: list.length ? virtualizer.getTotalSize() : "100%", position: "relative" }}>
-      {virtualizer.getVirtualItems().length === 0 ? (
-        emptyComponent || (
-          <div className="flex h-full w-full flex-row items-center justify-center gap-2 rounded-md border-2 border-(--border-color)/20 py-5 text-xl font-medium">
-            <RiErrorWarningLine className="mt-0.5" />
-            <div>No items to display</div>
-          </div>
-        )
-      ) : (
-        <AnimatePresence initial={false}>
-          {virtualizer.getVirtualItems().map((vItem) => (
-            <motion.div
-              key={vItem.key}
-              data-index={vItem.index}
-              ref={virtualizer.measureElement}
-              initial={{ opacity: 1, y: vItem.start }}
-              animate={{ opacity: 1, y: vItem.start }}
-              children={<Component index={vItem.index} />}
-              style={{ top: 0, left: 0, width: "100%", position: "absolute" }}
-              exit={{ opacity: 0, transition: { duration: 0.3, ease: "easeInOut" } }}
-            />
-          ))}
-        </AnimatePresence>
-      )}
+      {virtualizer.getVirtualItems().length === 0
+        ? emptyComponent || (
+            <div className="flex h-full w-full flex-row items-center justify-center gap-2 rounded-md border-2 border-(--border-color)/20 py-5 text-xl font-medium">
+              <RiErrorWarningLine className="mt-0.5" />
+              <div>No items to display</div>
+            </div>
+          )
+        : virtualizer
+            .getVirtualItems()
+            .map((vItem) => (
+              <div
+                key={vItem.key}
+                data-index={vItem.index}
+                ref={virtualizer.measureElement}
+                children={<Component index={vItem.index} />}
+                style={{ top: 0, left: 0, width: "100%", position: "absolute", transform: `translateY(${vItem.start}px)` }}
+              />
+            ))}
     </div>
   );
 
