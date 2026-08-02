747d47b00cca990045509629ae04e875a4301ef6
painfuego <kystax69@gmail.com>
2026-08-02 19:42:33 +0530

    lazy load thumb

--- src/renderer/components/utils/Card.tsx
+++ src/renderer/components/utils/Card.tsx
@@ -4,7 +4,7 @@ import { memo } from "react";
 
 export const Card = memo(({ thumb, label1, label2, onClick }: { thumb: string; label1: string; label2: string; onClick: () => void }) => (
   <div onClick={onClick} className="relative flex aspect-square h-full w-full cursor-pointer flex-col overflow-hidden rounded-md border-2 border-(--border-color)/20 active:scale-99">
-    <img src={thumb} decoding="async" draggable={false} className="absolute inset-0 -z-10 h-full w-full object-cover" />
+    <img src={thumb} loading="lazy" decoding="async" draggable={false} onError={(e) => (e.currentTarget.src = "./logo.png")} className="absolute inset-0 -z-10 h-full w-full object-cover" />
 
     <div className="flex h-full w-full flex-col items-end justify-end">
       <div className="flex h-12 w-full flex-col items-center gap-0.5 bg-(--hover-color)/50 p-2 backdrop-blur-sm">
