07494e8fa171e16c0a6dfd9689bda19e17ec7de1
painfuego <kystax69@gmail.com>
2026-08-02 20:24:42 +0530

    re-push all

--- src/renderer/index.tsx
+++
@@ -1,7 +0,0 @@
-/** @format */
-
-import "./index.css";
-import { Preload } from "./gates/Preload";
-import { createRoot } from "react-dom/client";
-
-createRoot(document.getElementById("root")!).render(<Preload />);
--- src/renderer/player/index.tsx
+++
@@ -1,5 +0,0 @@
-/** @format */
-
-export * from "./player";
-export * from "./volumeUnit";
-export * from "../stores/player";
